5f5e09ca6d78e5e951d2e83878f23d4eea0f6b99
AliDee <57602993+AliWithADee@users.noreply.github.com>
2021-12-14 13:55:43 +0000

    Delete Day10-1.py

--- Day 10/Day10-1.py
+++
@@ -1,3 +0,0 @@
-# Every time we find another opening character, append it to the list of opening characters
-# FIRST closing character should correspond with the LAST opening character
-# If it doesn't match, line is corrupt
